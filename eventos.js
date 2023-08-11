29fc053856f50d3fe91c25a277c61000b8ca2326
WashingtonCC <aa4549807@gmail.com>
2023-08-11 12:43:59 -0300

    Fixed propagation error

--- eventos.js
+++ eventos.js
@@ -1,5 +1,11 @@
 let container = document.querySelector(".btn-container");
+let btn = document.getElementById("btn");
 
 container.addEventListener("click", (e) => {
   alert("Hola! Soy el div");
+});
+
+btn.addEventListener("click", (e) => {
+  alert("Hola");
+  e.stopPropagation();
 });
