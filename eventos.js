ae8a834a2315d80ae6a9fdd65fb36de3fd75bb8f
WashingtonCC <aa4549807@gmail.com>
2023-08-11 12:47:05 -0300

    fixed missing files

--- eventos.js
+++ eventos.js
@@ -1,11 +1,5 @@
 let container = document.querySelector(".btn-container");
-let btn = document.getElementById("btn");
 
 container.addEventListener("click", (e) => {
   alert("Hola! Soy el div");
-});
-
-btn.addEventListener("click", (e) => {
-  alert("Hola");
-  e.stopPropagation();
 });
